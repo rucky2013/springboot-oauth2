6878da1b33146ba0f3949d2f3ce4baa2b815a446
KarpukDM <offspace@yandex.ru>
2016-10-05 23:53:59 +0300

    refactoring

---
+++ src/main/java/com/karpukdm/controllers/RequestController.java
@@ -0,0 +1,22 @@
+package com.karpukdm.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * Created by KarpukDM on 05.10.2016.
+ */
+@RestController
+public class RequestController {
+
+    @RequestMapping({ "/user", "/me" })
+    public Map<String, String> user(Principal principal) {
+        Map<String, String> map = new LinkedHashMap<>();
+        map.put("name", principal.getName());
+        return map;
+    }
+}
