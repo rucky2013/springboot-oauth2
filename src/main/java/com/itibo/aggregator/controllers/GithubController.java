c11802b75dfa4a0982484c27c9f6dc2d8fd6293a
KarpukDM <offspace@yandex.ru>
2016-10-07 19:22:11 +0300

    updated dependencies

--- src/main/java/com/itibo/aggregator/controllers/GithubController.java
+++ src/main/java/com/itibo/aggregator/controllers/GithubController.java
@@ -1,9 +1,5 @@
 package com.itibo.aggregator.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.social.connect.ConnectionRepository;
-import org.springframework.social.github.api.GitHub;
-import org.springframework.social.github.api.impl.GitHubTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,26 +11,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class GithubController {
 
-    private GitHub gitHub;
-
-    @Autowired
-    private ConnectionRepository connectionRepository;
-
-    public GithubController(GitHub gitHub) {
-        this.gitHub = gitHub;
-    }
-
-    public GithubController() {
-        this.gitHub = new GitHubTemplate();
-    }
-
     @RequestMapping(value = "/login/github", method = RequestMethod.GET)
     public String helloGithub(Model model) {
 
-        if (!this.gitHub.isAuthorized()) {
-
             return "redirect:/connect/github";
-        }
-        return "helloGithub";
     }
 }
